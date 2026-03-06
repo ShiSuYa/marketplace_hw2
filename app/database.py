375d21634054b981aabffe0923fec81c609a781f
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2026-03-06 12:45:16 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Замените на свои данные
 DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/marketplace"
 
 engine = create_engine(DATABASE_URL)
